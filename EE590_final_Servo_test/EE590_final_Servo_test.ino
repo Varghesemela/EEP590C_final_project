79041d268ff291f1fbc0617e22c4474a4f6183ed
sakalisi <sakalisi@uw.edu>
2025-06-02 11:25:37 -0700

    modified to include button press and not knob

--- EE590_final_Servo_test/EE590_final_Servo_test.ino
+++ EE590_final_Servo_test/EE590_final_Servo_test.ino
@@ -17,23 +17,24 @@
 //========= LIBRARIES =========
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-// #include <Wire.h>
+#include <Wire.h>
 #include <ESP32Servo.h>
-// #include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h>
 
 //========= PIN DEFINITIONS =========
-#define LED 1         ///< Output LED pin for anomaly alert
+#define LED 1  ///< Output LED pin for anomaly alert
 // #define LEDR 18       ///< Input photoresistor pin meant to read analog inputs
-#define SERVO_PIN 37  ///< Output pin for the servo motor
-#define POT_PIN 3
-// #define SDA_PIN 20  ///< I2C Data Pin
-// #define SCL_PIN 21  ///< I2C Clock Pin
+#define SERVO_PIN 12  ///< Output pin for the servo motor
+#define POT_PIN 17
+#define BUTTON_PIN 35
+#define SDA_PIN 20  ///< I2C Data Pin
+#define SCL_PIN 21  ///< I2C Clock Pin
 
 // //========= LCD SETUP =========
 // /**
 //  * @brief 16x2 I2C LCD at address 0x27
 //  */
-// LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo myservo;
 
 //========= TASK HANDLES =========
@@ -41,41 +42,44 @@ TaskHandle_t TaskLED_Handle = NULL;
 TaskHandle_t TaskServoRun_Handle = NULL;
 
 //========= GLOBAL VARIABLES =========
-static SemaphoreHandle_t semaphore;  ///< Semaphore for data synchronization
+// static SemaphoreHandle_t semaphore;  ///< Semaphore for data synchronization
 
 int currentAngle = 0;
+
+//Handling locking mechanism with buttons
+volatile bool isLock = false;
+bool lastButtonReading = HIGH;
+bool buttonState = HIGH;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 50;
 
 //========= SETUP =========
 /**
  * @brief Arduino setup function
  * @details 1. Initialize pins, serial, LCD, etc
- *          2. Create binary semaphore for synchronizing light level data.
- *          3. Create Tasks
- *          - Create the `Light Detector Task` and assign it to Core 0.
- *          - Create `LCD Task` and assign it to Core 0.
- *          - Create `Anomaly Alarm Task` and assign it to Core 1.
- *          - Create `Prime Calculation Task` and assign it to Core 1.
- *          4. Scheduler attempted before realizing that FreeRTOS automatically establishes a round robin system
- * Initializes peripherals, LCD, semaphore, and starts FreeRTOS tasks
  */
 void setup() {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
   pinMode(POT_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
 
   ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
   myservo.setPeriodHertz(50);
   myservo.attach(SERVO_PIN, 500, 2400);
   myservo.write(0);
   delay(50);
 
-  semaphore = xSemaphoreCreateBinary();
-  xSemaphoreGive(semaphore);
   xTaskCreatePinnedToCore(ServoRunTask, "servoRun", 2048, NULL, 1, &TaskServoRun_Handle, 1);
-  xTaskCreatePinnedToCore(LEDBlinkTask, "AnomalyBlink", 2048, NULL, 1, &TaskLED_Handle, 0);
 }
 
 
@@ -84,37 +88,41 @@ void setup() {
  * @brief Empty main loop since tasks handle all operations
  */
 void loop() {
-
 }
 
 //========= TASKS =========
+void updateButton() {
+  bool currentReading = digitalRead(BUTTON_PIN);
+
+  if (currentReading != lastButtonReading) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (currentReading != buttonState) {
+      if (buttonState == LOW && currentReading == HIGH) {
+        isLock = !isLock;
+      }
+      buttonState = currentReading;
+    }
+  }
+  lastButtonReading = currentReading;
+}
 
 void ServoRunTask(void *arg) {
   while (1) {
-    // xSemaphoreTake(semaphore, portMAX_DELAY);
-    currentAngle = analogRead(POT_PIN);
-    Serial.print("Current input read: " + String(currentAngle));
-    currentAngle = map(currentAngle, 0, 4096, 12, 180);  // scale it to use it with the servo (value between 0 and 180)
-    // xSemaphoreGive(semaphore);
+    updateButton();
 
-    Serial.println(". Corresponds to degrees: " + String(currentAngle));
-    myservo.write(currentAngle);    // set the servo position according to the scaled value
-    vTaskDelay(pdMS_TO_TICKS(10));  //creating a 0.5 second delay between each new read;                          // wait for the servo to get there
-  }
-}
-
-void LEDBlinkTask(void *arg) {
-  while (1) {
-    // xSemaphoreTake(semaphore, portMAX_DELAY);
-    if (currentAngle > 145 || currentAngle < 45) {
-      for (int i = 0; i < 4; i++) {
-        digitalWrite(LED, HIGH);
-        vTaskDelay(250);
-        digitalWrite(LED, LOW);
-        vTaskDelay(250);
-      }
+    if(isLock) {
+      myservo.write(180);
+      // lcd.clear();
+      // lcd.print("State: Lock");
+    } else {
+      myservo.write(0);
+      // lcd.clear();
+      // lcd.print("State: Unlock");
     }
-    // xSemaphoreGive(semaphore);
-    vTaskDelay(pdMS_TO_TICKS(100));
+    
+    vTaskDelay(pdMS_TO_TICKS(10));
   }
 }
